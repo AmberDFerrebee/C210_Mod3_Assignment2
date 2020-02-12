90e88aed570295f811d8862eb3d8ff9386b6edcb
Beth <bedamunk@iu.edu>
2020-02-12 13:10:06 -0600

    testing comment only
    testing comment only from ipad will remove later

--- Account.java
+++ Account.java
@@ -6,6 +6,7 @@ import java.text.*;
 // exercise 9.7
 // requirements:
 // write an Account class
+             // bethanie testing --- will remove
 			// private int data field named id, default 0
 			// private double data field named balance, default 0
 			// private double data named annualInterestRate, default 0
